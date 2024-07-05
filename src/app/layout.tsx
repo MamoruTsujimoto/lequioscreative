4b05ab4df598cd0a171f79420318b32e536d1eef
TsujimotoMamoru <tsujimotosirchristophermamoru@gmail.com>
2024-07-05 13:41:48 +0900

    Add: OGP Image
    Add: OGP Metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,9 @@ import "./globals.css";
 export const metadata: Metadata = {
   title: "Lequios Creative",
   description: "Frontend Portfolio From Okinawa",
+	openGraph: {
+		images: ['/ogp.jpg']
+	}
 };
 
 export default function RootLayout({
